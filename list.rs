fae34579c38ed4262fb67ef8ea3f44d32ea4d550
doug <linderd@iinet.net.au>
2014-03-12 10:41:40 +0800

    Fixed list I rock :yay:

--- list.rs
+++ list.rs
@@ -30,8 +30,21 @@ impl<T> Node<T> {
 
   /** Attach a node as the 'next' node in this chain */
   fn push<'a>(&'a mut self, value: T) -> &'a mut ~Node<T> {
-    self._next = Some(~Node::new(value));
+    if (self._next.is_none()) {
+      self._push_node(~Node::new(value));
+    }
+    else {
+      let tmp = self._next.take().unwrap();
+      let mut next = ~Node::new(value);
+      next._push_node(tmp);
+      self._push_node(next);
+    }
     return self._next.as_mut().unwrap();
+  }
+
+  /** Attach a raw node object as the 'next' node in this chain */
+  fn _push_node(& mut self, value: ~Node<T>) {
+    self._next = Some(value);
   }
 
   /** Get the 'next' node of this chain */
@@ -51,14 +64,14 @@ impl<T> Node<T> {
   }
 
   /** Apply some operator to each element in the list*/
-  fn each(&mut self, c:|value: & mut T|) {
-    Node::_applyEach(self.data(), |x| c(x));
+  fn each<U>(&mut self, context:& mut U, c:|context: & mut U, value: & mut T|) {
+    Node::_applyEach(context, self.data(), |x, y| c(x, y));
     let mut busy = true;
     let mut here = self.next();
     while busy {
       match here {
-        Ok(mut e) => {
-          Node::_applyEach(e.data(), |x| c(x));
+        Ok(e) => {
+          Node::_applyEach(context, e.data(), |x, y| c(x, y));
           here = e.next();
         },
         _ => busy = false
@@ -67,11 +80,32 @@ impl<T> Node<T> {
   }
 
   /** Actually apply an each function to a data point */
-  fn _applyEach(r:Result<& mut T, NodeErr>, c:|value: & mut T|) {
+  fn _applyEach<U>(context:& mut U, r:Result<& mut T, NodeErr>, c:|context:& mut U, value: & mut T|) {
     match r {
-      Ok(e) => { c(e) },
+      Ok(e) => { 
+        trace!("Walking down node: {:?}", e);
+        c(context, e) 
+      },
       _ => {}
     }
+  }
+
+  /** Walk the entire chain and count values */
+  fn count(&mut self) -> int {
+    let mut count = 0;
+    self.each(& mut count, |c:& mut int, _:& mut T| {
+      *c += 1;
+    });
+    return count;
+  }
+
+  /* Return an immutable filtered set of values */
+  fn count_filtered(&mut self, c:|value: & T| -> bool) -> int {
+    let mut count = 0;
+    self.each(& mut count, |c:& mut int, _:& mut T| {
+      *c += 1;
+    });
+    return count;
   }
 }
 
@@ -85,7 +119,16 @@ fn test_create_node() {
 fn test_create_node_chain() {
   let mut x = ~Node::<int>::blank();
   x.push(10).push(11).push(12).push(13);
-  x.each(|value:& mut int| {
-    trace!("Got value: {:?}", value);
-  });
+  trace!("Found a total of {:?} values", x.count());
+  assert!(x.count() == 4);
+}
+
+#[test]
+fn test_count_filtered_node_chain() {
+  trace!("test_count_filtered_node_chain -->");
+  let mut x = ~Node::<int>::blank();
+  for i in range(0, 10) {
+    x.push(i);
+  }
+  trace!("test_count_filtered_node_chain: Found a total of {:?} values", x.count());
 }
