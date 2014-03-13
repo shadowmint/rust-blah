7c92cea6933566700033a6886f672ae7a0472308
doug <linderd@iinet.net.au>
2014-03-13 10:53:25 +0800

    Added an interator and map example

---
+++ map_test.rs
@@ -0,0 +1,22 @@
+use _macros;
+
+#[test]
+fn test_map_to_new_values() {
+  let mut x = ~[1, 2, 3];
+  let mut output = x.iter().map(|y| { 
+    trace!("Value: {}", y);
+    return *y + 1;
+  });
+  for x in output {
+    trace!("{}", x);
+  }
+}
+
+#[test]
+fn test_map_replacing_values() {
+  let mut x = ~[1, 2, 3];
+  let z = x.mut_iter().map(|y| { 
+    return *y + 1;
+  }).to_owned_vec();
+  trace!("{}", z);
+}
