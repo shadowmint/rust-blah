48cc8d4e7e5c05674faac5ddef35796ae38ee114
doug <linderd@iinet.net.au>
2015-01-25 18:33:46 +0800

    adds example debug implementation

---
+++ src/show_debug.rs
@@ -0,0 +1,17 @@
+use std::fmt;
+
+struct BlahLF {
+    id: usize
+}
+
+impl fmt::Debug for BlahLF {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Hi: {}", self.id)
+    }
+}
+
+#[test]
+fn test_show() {
+    let foo = BlahLF { id: 0us };
+    println!("{:?}", foo);
+}
