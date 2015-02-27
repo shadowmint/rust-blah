7e67f29b4166f1986b8f655418b9f55e5c444b43
doug <linderd@iinet.net.au>
2015-02-27 10:28:49 +0800

    Added higher kinded types to static trait methods example

--- src/static_trait_methods.rs
+++ src/static_trait_methods.rs
@@ -1,14 +1,31 @@
+use std::any::TypeId;
+
 trait Foo : ::std::marker::MarkerTrait {
+  type Item: 'static;
   fn foo() -> isize {
     return 1;
   }
 }
 
+struct Bar;
 struct Foo1;
 
-impl Foo for Foo1 {}
+impl Foo for Foo1 {
+  type Item = Bar;
+}
+
+impl Foo1 {
+  fn s<T: 'static>(&self) -> bool where Self: Foo {
+    let rtn = TypeId::of::<<Self as Foo>::Item>() == TypeId::of::<T>();
+    return rtn;
+  }
+}
 
 #[test]
 fn test_methods() {
-  println!("Hello {:?}!", <Foo1 as Foo>::foo());
+  trace!("Hello {:?}!", <Foo1 as Foo>::foo());
+
+  let f = Foo1;
+  trace!("World {:?}!", f.s::<Foo1>());
+  trace!("World {:?}!", f.s::<Bar>());
 }
